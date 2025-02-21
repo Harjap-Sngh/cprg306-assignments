daef1ff5bbf6a4ddd872182758d02243f2e774b3
Harjap Singh <157319482+Harjap-Sngh@users.noreply.github.com>
2025-02-20 17:55:04 -0700

    installed fire base

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -1,12 +1,11 @@
-'use client'
-import React from 'react'
-import { useState } from 'react';
+"use client";
+import React from "react";
+import { useState } from "react";
 
 const NewItem = () => {
-
-    let [quantity, setQuantity] = useState(1);
-    let [category, setCategory] = useState('produce');
-    let [itemName, setItemName] = useState('');
+  let [quantity, setQuantity] = useState(1);
+  let [category, setCategory] = useState("produce");
+  let [itemName, setItemName] = useState("");
 
   const increaseQuantity = () => {
     setQuantity(quantity + 1);
@@ -19,13 +18,16 @@ const NewItem = () => {
   };
 
   const addItem = () => {
-    if(itemName === '') {}else{
-    alert(`Added item: ${itemName}, quantity:${quantity} , category: ${category}`);
-    quantity = 1;
-    category = 'produce';
-    itemName = '';}
+    if (itemName === "") {
+    } else {
+      alert(
+        `Added item: ${itemName}, quantity:${quantity} , category: ${category}`
+      );
+      quantity = 1;
+      category = "produce";
+      itemName = "";
+    }
   };
-
 
   return (
     <form className="flex items-center space-x-4 text-black">
@@ -35,9 +37,8 @@ const NewItem = () => {
         required
         onChange={(e) => setItemName(e.target.value)}
       />
-      
+
       <div className="flex items-center bg-white rounded-md text-black h-10">
-        
         <input
           type="number"
           value={quantity}
@@ -45,6 +46,7 @@ const NewItem = () => {
           className="text-center"
         />
         <button
+          type="button"
           onClick={increaseQuantity}
           className="p-1 w-8 bg-blue-600 hover:bg-blue-700 rounded-md mr-1"
         >
@@ -52,15 +54,27 @@ const NewItem = () => {
         </button>
         <button
           onClick={decreaseQuantity}
-          className={`p-1 w-8 rounded-md mr-1 ${quantity<=1? 'bg-blue-400 hover:bg-blue-400 cursor-not-allowed' : ' bg-blue-600 hover:bg-blue-700'}`}
-          disabled={quantity<=1? true : false}
+          type="button"
+          className={`p-1 w-8 rounded-md mr-1 ${
+            quantity <= 1
+              ? "bg-blue-400 hover:bg-blue-400 cursor-not-allowed"
+              : " bg-blue-600 hover:bg-blue-700"
+          }`}
+          disabled={quantity <= 1 ? true : false}
         >
           -
         </button>
       </div>
-      <select className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans text-black" onChange={(e) => setCategory(e.target.value)}>
-        <option value="" disabled>Category</option>
-        <option value="produce" defaultValue>Produce</option>
+      <select
+        className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans text-black"
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="" disabled>
+          Category
+        </option>
+        <option value="produce" defaultValue>
+          Produce
+        </option>
         <option value="dairy">Dairy</option>
         <option value="bakery">Bakery</option>
         <option value="meat">Meat</option>
@@ -71,15 +85,16 @@ const NewItem = () => {
         <option value="snacks">Snacks</option>
         <option value="household">Household</option>
         <option value="other">Other</option>
-       </select>
-        <button
-          className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-md" onClick={addItem}>
+      </select>
+      <button
+        className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-md"
+        type="button"
+        onClick={addItem}
+      >
         Add Item
-        </button>
+      </button>
     </form>
-    
+  );
+};
 
-  )
-}
-
-export default NewItem
+export default NewItem;
