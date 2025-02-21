80904cb936104c4eba1b4cc02a83329b3f322363
Harjap Singh <157319482+Harjap-Sngh@users.noreply.github.com>
2025-02-20 20:26:22 -0700

    tailwind done for week -9

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -26,32 +26,27 @@ const Page = () => {
   }
 
   // Sign out of Firebase
-  async function logout() {
-    setLoading(true);
-    try {
-      await firebaseSignOut();
-    } catch (error) {
-      console.error("Sign-Out Error:", error);
-    } finally {
-      setLoading(false);
-    }
-  }
 
   return (
     <div>
-      <h1>Hi there, Login is required for access</h1>
       {!user && (
-        <button onClick={login} disabled={loading}>
-          {loading ? "Logging in..." : "Login with GitHub"}
-        </button>
+        <>
+          <h1 className="text-center mt-10 text-5xl">
+            Hi there, Login is required for access
+          </h1>
+          <button
+            onClick={login}
+            disabled={loading}
+            className="border-2 border-white rounded px-5 py-1 text-white hover:text-black hover:bg-white m-auto block mt-5"
+          >
+            {loading ? "Logging in..." : "Login with GitHub"}
+          </button>
+        </>
       )}
       <div>
         {user && (
           <div>
             <h1>Welcome {user.displayName}</h1>
-            <button onClick={logout} disabled={loading}>
-              {loading ? "Logging out..." : "Logout"}
-            </button>
             <Link href="/week-9/shopping-list"> Go to Shopping List</Link>
           </div>
         )}
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -3,9 +3,20 @@
 import React from "react";
 import NewItem from "./newItem";
 import { useUserAuth } from "../_utils/auth-context";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  const { user } = useUserAuth();
+  const { user, firebaseSignOut } = useUserAuth();
+  const router = useRouter();
+
+  async function logout() {
+    try {
+      await firebaseSignOut();
+      router.push("/week-9");
+    } catch (error) {
+      console.error("Sign-Out Error:", error);
+    }
+  }
 
   return (
     <>
@@ -13,6 +24,12 @@ const Page = () => {
         <main className="flex flex-col items-center space-y-4 my-4">
           <h1 className="text-5xl">Shopping List</h1>
           <NewItem />
+          <button
+            onClick={logout}
+            className="absolute right-0 mr-9 border-white border-2 rounded px-5 py-1 text-white hover:text-black hover:bg-gray-200"
+          >
+            Logout
+          </button>
         </main>
       ) : (
         <main className="flex flex-col items-center space-y-4 my-4">
